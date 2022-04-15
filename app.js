1069db734dd8bae2c10bf4918542dd5c4601ab58
hya-cala <Rena.Yang39@gmail.com>
2022-04-15 02:38:31 -0400

    basic REST structure

--- app.js
+++ app.js
@@ -1,12 +1,66 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+const methodOverride = require('method-override')
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', () => {
+    console.log('Database connected');
+})
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'));
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.render('home');
 })
 
-app.listen(3000, ()=>console.log('Serving on Port 3000'))
+app.get('/campground', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds })
+})
+
+app.get('/campground/new', (req, res) => {
+    res.render('campgrounds/new')
+})
+
+app.post('/campground', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campground/${campground._id}`);
+})
+
+
+app.get('/campground/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render('campgrounds/show', { campground })
+})
+
+app.get('/campground/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render('campgrounds/edit', { campground })
+})
+app.put('/campground/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
+    res.redirect(`/campground/${campground._id}`);
+})
+app.delete('/campground/:id', async(req, res)=>{
+    const {id} = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campground')
+})
+
+app.listen(3000, () => console.log('Serving on Port 3000'))
